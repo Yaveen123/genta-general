7c6531e8b5d8643a5a253acaa2064f2c976eb288
Yaveen! <94953863+Yaveen123@users.noreply.github.com>
2025-04-26 10:08:46 +1000

    added google sign in (may break things)

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,6 +6,8 @@ def home():
     return render_template("index.html")
 # Define your database models here
 
+#Only runs in flask not gunicorn
 if __name__ == '__main__':
     # Example of creating tables (if they don't exist)
-    app.run(debug=True)
+    
+    app.run(debug=True, host='localhost', port=6969)
