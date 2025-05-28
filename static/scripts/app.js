fce9d83f0714796dad7a9628686be3ba35c22d3a
Yaveen! <94953863+Yaveen123@users.noreply.github.com>
2025-05-28 22:52:52 +1000

    Added JWT verification on genta.live/app

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -25,3 +25,41 @@ document.addEventListener('DOMContentLoaded', function() {
         signOutButton.addEventListener('click', signOut);
     }
 });
+
+window.onload = function () {
+    if (localStorage.getItem('idToken') === null || localStorage.getItem('idToken') === 'NULL') {
+        window.location.replace("/");
+    } else {
+        verifyLogin().then(isValid => {
+            if (isValid) {
+                document.getElementById('loading-animation').style.display = "None";
+                document.getElementById('signOutButton').style.display = "Block";
+            } else {
+                window.location.replace("/");
+            }
+        });
+    }
+}
+
+
+async function verifyLogin() {
+    const idToken = localStorage.getItem('idToken');
+    const url = "https://genta-api.online/verify-login";
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + idToken,
+            }
+        });
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`)
+        }
+        const json = await response.json();
+        
+        return true; // Verification successful
+    } catch (error) {
+        console.error(error.message);
+        return false; // Verification failed
+    }
+}
