273d2c5fec1cb975fec2a0f96c8df38d75ddaa1c
Yaveen! <94953863+Yaveen123@users.noreply.github.com>
2025-06-03 10:43:55 +1000

    force only one item to be in edit mode at any given time

--- static/scripts/app/event-edit.js
+++ static/scripts/app/event-edit.js
@@ -4,9 +4,16 @@ document.addEventListener('DOMContentLoaded', function() {
 
         // for every element, if the settings button has been clicked, change everythin to edit mode
         element.querySelector(".event_title__settings-icon").addEventListener("click", function() {
+            // remove edit mode from all other event-outer-boxes
+            document.querySelectorAll('.event-outer-box').forEach(box => {
+                box.querySelector(".event").classList.remove('event--in-edit-mode');
+                box.querySelector(".edit-box").classList.remove('edit-box--in-edit-mode');
+            });
+            // add edit mode to the current one
             element.querySelector(".event").classList.add('event--in-edit-mode');
             element.querySelector(".edit-box").classList.add('edit-box--in-edit-mode');
         });
+
 
         // for every element, if the cancel button has been clicked, change everything to not edit mode 
         element.querySelector(".edit-box__cancel-box").addEventListener("click", function() {
