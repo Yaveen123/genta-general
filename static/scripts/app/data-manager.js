a834b66cbed9998d307b5a1407ca5e62237365fd
Yaveen! <94953863+Yaveen123@users.noreply.github.com>
2025-06-03 19:47:38 +1000

    added retrieving data and assigning ids

--- static/scripts/app/data-manager.js
+++ static/scripts/app/data-manager.js
@@ -117,11 +117,93 @@ document.addEventListener('DOMContentLoaded', async function() {
         console.log('TEST CASE 2 FAILED: unspecified error')
         return
     }
+
+    if (!await loadInitialUserData()) {
+        console.log("TEST CASE 3 FAILED -> Unable to get user data")
+        return
+    } else {
+        console.log("TEST CASE 3 PASSED -> able to get user data")
+    }
 });
 
 // MARK: generateID()
 // How can I create unique IDs with JS? https://stackoverflow.com/questions/3231459/how-can-i-create-unique-ids-with-javascript
 function generateID() {
     return crypto.randomUUID();
+}
+
+// MARK: assignTempIds()
+// Process projects and their nested items for temp IDs if necessary
+function assignTempIds(items) {
+    // Just in case i nee d to check if it's an array and i can do that using Array.isArray()
+    // "fela" (2012) How can I check if an object is an array? https://stackoverflow.com/questions/4775722/how-can-i-check-if-an-object-is-an-array 
+    if (!Array.isArray(items)) {
+        console.error('assignTempIds > returned data is not an array')
+        return
+    };
+    
+    // use __ for temp/internal tasks
+    // loop over each item
+    for (let i = 0; i < items.length; i++) {
+        const item = items[i];
+        if (!item.id && !item.__tempId) { // if the item does not have an ID or a temp id
+            item.__tempId = generateID();
+        }
+        
+        if (item.events) { // potentially complex logic, but if item has events then it can treat it like a project and loop back into the function to assign ids
+            // this works because event's dont have subevents.
+            assignTempIds(item.events); 
+
+            // loop over each event
+            for (let j = 0; j < item.events.length; j++) {
+                if (item.events[j].todo) {
+                    // loop through again but for todos
+                    assignTempIds(item.events[j].todo);
+                }
+            }
+        }
+    }
+}
+
+// This is the first load of data 
+async function loadInitialUserData() {
+    if (!getAuthToken()) {
+        // This should never happen, there are many tests before this, and this cant happen unless someone is tampering with the client
+        console.log('loadInitialUserData > No auth token present. How did you get here?');
+        return;
+    }
+
+    document.getElementById('loading-animation').style.display = 'block'; // show the loading anim
+
+    try {
+        // try to get user data
+        const data = await fetchAPI('/get-data');
+        if (!data) {
+            console.error('loadInitialUserData > Failed to fetch user data:', response);
+            return
+        }
+
+        // potentially null if it's a new user
+        currentUserData.user_db_id = data.user_db_id;
+        currentUserData.user_version_tag = data.user_version_tag;
+        localStorage.setItem('gentaUserVersionTag', data.user_version_tag);
+
+        if (data.projects) { // if there are projects we don't want a false we want a empty list so iterators can handle this well
+            currentUserData.projects = data.projects;
+        } else {
+            currentUserData.projects = [];
+        }
+
+        // assign the temporary ids
+        assignTempIds(currentUserData.projects);
+
+        console.log("loadInitialUserData > User data loaded: " + JSON.stringify(currentUserData))
+        return true;
+    } catch (error) {
+        console.error("loadInitialUserData > Error in loading data: " + error)
+        return false;
+    } finally {
+        document.getElementById('loading-animation').style.display = 'none'
+    }
 }
 
